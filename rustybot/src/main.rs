aed8beed8c60fa8ffa122656ef7c23a6b4c3faaa
rookmate <14072042+rookmate@users.noreply.github.com>
2020-03-03 17:32:47 +0000

    [TWITCHBOT] New account name. Reply to @ and one command.

--- rustybot/src/main.rs
+++ rustybot/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     let oauth_token = env::var("TWITCH_OAUTH_TOKEN").unwrap();
 
     let cfg = Config {
-        nickname: Some("RustyBot".to_owned()),  // Bot name
+        nickname: Some("rusty_morpho".to_owned()),  // Bot name
         server: Some("irc.chat.twitch.tv".to_owned()),
         port: Some(6667),
         password: Some(oauth_token),    // Token from oauth token to tmi
@@ -22,5 +22,12 @@ fn main() {
 
     irc_client.for_each_incoming(|irc_msg| {
         println!("Incoming <<< {}", irc_msg);
+        if let Command::PRIVMSG(channel, message) = irc_msg.command {
+            if message.contains(irc_client.current_nickname()) {
+                irc_client.send_privmsg(&channel, "Reporting for duty!").unwrap();
+            } else if message.contains("!help") {
+                irc_client.send_privmsg(&channel, "I wish I could help you sir.").unwrap();
+            }
+        }
     }).unwrap();
 }
