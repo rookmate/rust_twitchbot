b70b319e47aad43ebc1ba3f5fcfda45d6cc70e5b
rookmate <14072042+rookmate@users.noreply.github.com>
2020-03-03 17:42:50 +0000

    [TWITCHBOT] Implement with IrcReactor for multiserver support

--- rustybot/src/main.rs
+++ rustybot/src/main.rs
@@ -6,7 +6,7 @@ use irc::client::prelude::*;
 
 fn main() {
     let oauth_token = env::var("TWITCH_OAUTH_TOKEN").unwrap();
-
+    // We can also load the Config at runtime via Config::load("path/to/config.toml")
     let cfg = Config {
         nickname: Some("rusty_morpho".to_owned()),  // Bot name
         server: Some("irc.chat.twitch.tv".to_owned()),
@@ -16,18 +16,22 @@ fn main() {
         ..Default::default()
     };
 
-    let irc_client = IrcClient::from_config(cfg).unwrap();
-
+    let mut reactor = IrcReactor::new().unwrap();
+    let irc_client = reactor.prepare_client_and_connect(&cfg).unwrap();
     irc_client.identify().unwrap();
 
-    irc_client.for_each_incoming(|irc_msg| {
-        println!("Incoming <<< {}", irc_msg);
-        if let Command::PRIVMSG(channel, message) = irc_msg.command {
-            if message.contains(irc_client.current_nickname()) {
-                irc_client.send_privmsg(&channel, "Reporting for duty!").unwrap();
-            } else if message.contains("!help") {
-                irc_client.send_privmsg(&channel, "I wish I could help you sir.").unwrap();
+    reactor.register_client_with_handler(irc_client, |client, message| {
+        println!("Incoming <<< {}", message);
+        if let Command::PRIVMSG(ref channel, ref msg) = message.command {
+            if msg.contains(client.current_nickname()) {
+                client.send_privmsg(channel, "Reporting for duty!").unwrap();
+            } else if msg.contains("!help") {
+                client.send_privmsg(channel, "I wish I could help you sir.").unwrap();
             }
         }
-    }).unwrap();
+
+        Ok(())
+    });
+
+    reactor.run().unwrap();
 }
